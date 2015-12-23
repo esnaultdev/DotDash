6b9e1a6c5700b7a24e3c4be97d5e8a00376f5fae
Ao <aomeas@gmail.com>
2015-12-23 23:40:18 +0100

    Code sheet close button

--- app/src/main/java/net/aohayo/dotdash/morse/CodeSheetFragment.java
+++ app/src/main/java/net/aohayo/dotdash/morse/CodeSheetFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.GridView;
 
 import net.aohayo.dotdash.R;
@@ -30,6 +31,13 @@ public class CodeSheetFragment extends DialogFragment {
                 GridView listView = (GridView) alertDialog.findViewById(R.id.code_list);
                 CodeSheetAdapter adapter = new CodeSheetAdapter(context, new MorseCodec(context, R.xml.morse_code_itu));
                 listView.setAdapter(adapter);
+                View closeButton = alertDialog.findViewById(R.id.image_view_close);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CodeSheetFragment.this.dismiss();
+                    }
+                });
             }
         });
 
