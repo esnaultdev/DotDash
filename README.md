4498f67a50a4ee853410f4a88b482ded891e8943
Ao <aomeas@gmail.com>
2016-01-17 14:00:23 +0100

    Added Crashlytics

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Screenshots
 &nbsp;
 <img src="http://aohayo.net/apps/DotDash/io_menu.png" height="640">
 
+Build
+-----
+As DotDash uses Crashlytics, you must provide your API key in api-keys.properties at the root of the project.
+```
+crashlytics=youApiKey
+```
+
 License
 -------
 DotDash is under the GPL v2 license.
--- app/src/main/java/net/aohayo/dotdash/main/MainActivity.java
+++ app/src/main/java/net/aohayo/dotdash/main/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import net.aohayo.dotdash.R;
 
 import net.aohayo.dotdash.inputoutput.IOActivity;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 
